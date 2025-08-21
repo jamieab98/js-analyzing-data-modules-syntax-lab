fcb3b07d1a20661d47ba5d6a8fd8324a5e952d1c
Jamie Bryson <jamieab98@gmail.com>
2025-08-21 18:15:09 -0400

    actually used to spread operator and the provided test file

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-function combineUsers(args) {
+function combineUsers(...args) {
+  console.log(args);
   let combineObject = {users: []};
   let a = 0;
+  
   while (a < args.length) {
     let tempUser = args[a];
-    combineObject.users.push(tempUser);
+    combineObject.users = [...combineObject.users, ...tempUser];
+    console.log(combineObject.users);
     a++
   }
-  console.log(combineObject);
+
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const month = currentDate.getMonth();
+  const day = currentDate.getDay();
+  const merge_date = `${month}/${day}/${year}`;
+
+  combineObject.merge_date = merge_date;
+  //console.log(combineObject);
   return combineObject;
 }
-let input = ['Jamie', 'Bobby', 'Michael'];
 
-combineUsers(input)
+combineUsers(["Jim3","Pam5","Dwight77"],["Michael6","Eleanor22","Chidi202"],["Jack_jack","Julia_Oreo", "Bill_bore"])
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
