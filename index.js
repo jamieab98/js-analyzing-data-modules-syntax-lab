17e47847eb6415a250246229b7798b5924527860
Jamie Bryson <jamieab98@gmail.com>
2025-08-21 17:39:21 -0400

    successfully made it so the function would combine the arrays into the users property

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 function combineUsers(args) {
   let combineObject = {users: []};
   let a = 0;
-  for (let arg of args) {
-    console.log(arg);
+  while (a < args.length) {
+    let tempUser = args[a];
+    combineObject.users.push(tempUser);
+    a++
   }
-  return args;
+  console.log(combineObject);
+  return combineObject;
 }
-let input = ['Jamie', 'Bobby'];
+let input = ['Jamie', 'Bobby', 'Michael'];
 
 combineUsers(input)
-
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
